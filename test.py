d8733cde055b30d5d5b0c3ef48547879775edf90
Jason Chen <jnchen12a@gmail.com>
2022-10-26 19:46:22 -0400

    Completed encoding function, added checks for user input

--- test.py
+++ test.py
@@ -1,31 +1,49 @@
 # Encoding function
 def encode(num):
-    pass
+    return_num = ''
+    for digit in num:
+        return_num = str(int(digit) + 3) + return_num
+
+    return return_num
 # Decoding function
 def decode(num):
     pass
 
 # Start of main if dunder
 if __name__ == '__main__':
+    encoded_password = False
     # Start of main while loop
     while True:
+        # Printing menu
         print('Menu')
         print('-----------')
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
         print()
-        choice = int(input('Please enter an option: '))
-        if choice == 1: # Encode a password
-            password = int(input('Please enter your password to encode: '))
-            encoded_password = encode(password)
-            print('Your password has been encoded and stored!')
-        elif choice == 2: # Decode a password
+        choice = input('Please enter an option: ')
+        if choice == '1': # Encode a password
+            while True:
+                try:
+                    password = input('Please enter your password to encode: ')
+                    encoded_password = encode(password)
+                    print('Your password has been encoded and stored!')
+                    print()
+                    break
+                except: # Bozo check, user enters non-number
+                    print('Please enter a numerical password')
+        elif choice == '2': # Decode a password
+            if not encoded_password: # Bozo check, nothing to decode
+                print('No password to decode.')
+                print()
+                continue
             decoded_password = decode(encoded_password)
             print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.')
-        elif choice == 3: # Quitting out
+            print()
+        elif choice == '3': # Quitting out
             quit()
-        else: # Bozo check
+        else: # Bozo check, user doesn't choose a valid choice
             print('Please enter a valid choice.')
+            print()
     # End of main while loop
 # End of main if dunder
